086ad805fc36968c8592a820f348d67618fe4796
winningchampcodder <laveztevez2004@gmail.com>
2023-02-17 09:31:26 -0800

    trilking white space

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
  * main - main block
  * Description: Get a random number and print the number
  * and if it is positive, negative, or zero
- * Return 0
+ * Return: Always 0 (Success) 
  */
 int main(void)
 {
