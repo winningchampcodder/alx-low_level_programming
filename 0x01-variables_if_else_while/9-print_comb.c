bbf40f3e7943ccf5a07d2bb8630c5487d2a90759
winningchampcodder <laveztevez2004@gmail.com>
2023-02-19 11:40:52 -0800

    persistent

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: patience persistence and perspiration for unbeatable success
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		c++
+	}
+
+	putchar('\n');
+	return (0);
+}
