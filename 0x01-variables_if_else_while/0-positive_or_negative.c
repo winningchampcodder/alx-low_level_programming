fddd49d74b7a29595d46d29a1c64cbb60a6769dd
winningchampcodder <laveztevez2004@gmail.com>
2023-02-18 22:51:33 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - main block
  * Description: Get a random number and print the number if it is positive, negative or zero
- * Return: 0 
+ * Return: 0 (Success) 
  */
 int main(void)
 {
