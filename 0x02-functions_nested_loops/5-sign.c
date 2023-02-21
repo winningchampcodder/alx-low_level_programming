e33c55c96e7dda81fddbe401883d128745904208
winningchampcodder <laveztevez2004@gmail.com>
2023-02-21 07:13:08 -0800

    last attempt

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: function parameter
+ *
+ * Return: 1 and print + if n is greater than zero
+ * 0 and prints 0 if n zero
+ * -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else  
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
