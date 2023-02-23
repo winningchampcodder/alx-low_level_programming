4a39cdd19ad370f0e8d84e82d24b868b1eed5a65
winningchampcodder <laveztevez2004@gmail.com>
2023-02-23 05:54:05 -0800

    multiplication of integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - checks for digits 0 through 9
+ * @a: a - variable
+ * @b: b - variable
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	 mul = a * b;
+
+	return (mul);
+}
