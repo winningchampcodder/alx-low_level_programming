501e0badc2d89042698cc5eef7a3eb1f44e53d7c
winningchampcodder <laveztevez2004@gmail.com>
2023-03-16 10:58:47 -0700

    task 4

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,15 +11,15 @@
 
 int *array_range(int min, int max)
 {
-	int *ptr;
-	int i, size;
+int *ptr;
+int i, size;
 
-	if (min > max)
-		return (NULL);
+if (min > max)
+return (NULL);
 
-	size = max - min + 1;
+size = max - min + 1;
 
-	ptr = malloc(sizeof(int) * (size);
+ptr = malloc(sizeof(int) * (size);
 
 if (ptr == NULL)
 return (NULL);
